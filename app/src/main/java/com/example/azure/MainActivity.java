f99a1687b7b77eec1625543b1f0e818cf2e04f5a
Jia Yu Lin <purpleinklingyu@gmail.com>
2019-03-23 21:26:24 -0400

    first commit

---
+++ app/src/main/java/com/example/azure/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.azure;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void SelfHelp(View view) {
+        Intent i = new Intent ( this, MainActivity4.class);
+        startActivity(i);
+
+
+    }
+}
